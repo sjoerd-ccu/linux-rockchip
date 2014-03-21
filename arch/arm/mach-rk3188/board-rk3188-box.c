22b47696b82f5684311c5018bbd78ddf35f5d294
Tom Cubie <tom@radxa.com>
2014-03-21 18:14:20 +0800

    radxa: change to the right reset pin of rk1000

--- arch/arm/mach-rk3188/board-rk3188-box.c
+++ arch/arm/mach-rk3188/board-rk3188-box.c
@@ -1306,7 +1306,7 @@ static struct rkdisplay_platform_data tv_data = {
 	.property 		= DISPLAY_MAIN,
 	.video_source 	= DISPLAY_SOURCE_LCDC0,
 	.io_pwr_pin 	= INVALID_GPIO,
-	.io_reset_pin 	= RK30_PIN3_PD4,
+	.io_reset_pin 	= RK30_PIN3_PD7,
 	.io_switch_pin	= RK30_PIN2_PD7,
 };
 #endif
